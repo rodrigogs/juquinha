f0618925078e66d3d1dc12ef251676536a18a26f
rodrigogs <rodrigo.smscom@gmail.com>
2022-01-23 22:45:24 -0300

    fixing github actions

--- modules/api/users/delete.js
+++ modules/api/users/delete.js
@@ -1,5 +1,5 @@
-import removeUser from 'lib/services/users/remove'
-import removeUserRole from 'lib/services/user-roles/remove'
+import UsersService from 'lib/services/users'
+import UserRolesService from 'lib/services/user-roles'
 import { default as Router, responseBuilder } from 'lib/helpers/router'
 
 export const handler = async (event, context) => {
@@ -7,12 +7,12 @@ export const handler = async (event, context) => {
 
     .delete('/users/:id', (request) => {
       const { pathParameters } = request
-      return removeUser(pathParameters.id).then(() => responseBuilder.success.noContent())
+      return UsersService.remove(pathParameters.id).then(() => responseBuilder.success.noContent())
     })
 
     .delete('/users/:id/roles/:roleId', (request) => {
       const { pathParameters } = request
-      return removeUserRole({
+      return UserRolesService.remove({
         userId: pathParameters.id,
         roleId: pathParameters.roleId,
       }).then(() => responseBuilder.success.noContent())
