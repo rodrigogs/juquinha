b3dff4c423af0e7dc92bd5478542f50586afa0d5
rodrigogs <rodrigo.smscom@gmail.com>
2022-01-25 12:46:53 -0300

    updated serverless

--- modules/web/pages/dashboard.vue
+++ modules/web/pages/dashboard.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
 v-container
   v-row
-    v-col Test
+    v-col
+      h1 Dashboard
+      h2 Welcome to your dashboard
 </template>
 
+<script>
+import { defineComponent as dc } from '#imports'
+
+export default dc({
+  name: 'Dashboard',
+  data() {
+    return {
+      Test: 'Test'
+    }
+  }
+})
+</script>
