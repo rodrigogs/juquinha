351b0dbeab769148758aebaa7b9c23ea44d133d2
rodrigogs <rodrigo.smscom@gmail.com>
2022-01-29 14:24:46 -0300

    some fixes

--- scripts/actions/test/integration-tests.js
+++ scripts/actions/test/integration-tests.js
@@ -3,7 +3,7 @@ const runNpmScript = require('lib/helpers/run-npm-script')
 
 async function run () {
   try {
-    await runNpmScript('test:integration', true)
+    await runNpmScript('test:integration')
     process.exit(0)
   } catch (error) {
     core.setFailed(error.message)
