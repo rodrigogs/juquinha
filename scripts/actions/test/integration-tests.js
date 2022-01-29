debc2afc8d38786d817a45e9da022d9c7a235537
rodrigogs <rodrigo.smscom@gmail.com>
2022-01-29 18:47:03 -0300

    trying to fix github actions

--- scripts/actions/test/integration-tests.js
+++ scripts/actions/test/integration-tests.js
@@ -1,10 +1,29 @@
 const core = require('@actions/core')
 const runNpmScript = require('lib/helpers/run-npm-script')
+const chalk = require('chalk')
 
 async function run () {
   try {
-    await runNpmScript('test:integration')
-    process.exit(0)
+    const stdout = await runNpmScript('test:integration')
+    // FIXME: this is a hack to get the output of the test script.
+    // Even when all the tests passes, in the ci environment, the process returns a non-zero code.
+    //
+    // check if stdout has a line that contains 'Tests:       {x} passed, {x} total'
+    // where {x} is a number and must be equal for passed and total
+    const regex = '/Tests:       (\\d+) passed, (\\d+) total/'
+    const match = stdout.match(regex)
+    if (match) {
+      const passed = Number(match[1])
+      const total = Number(match[2])
+      if (passed === total) {
+        core.setOutput('passed', passed)
+        core.setOutput('total', total)
+        process.exit(0)
+      } else {
+        core.setFailed(`Only ${chalk.yellow(passed)} of ${chalk.red(total)} tests passed.`)
+      }
+    }
+    core.setFailed('Failed to parse stdout')
   } catch (error) {
     core.setFailed(error.message)
   }
