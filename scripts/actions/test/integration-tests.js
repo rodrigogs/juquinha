e033663f239e2c95b0da961723a5dda2d687a15d
rodrigogs <rodrigo.smscom@gmail.com>
2022-01-29 19:52:01 -0300

    trying to fix github actions

--- scripts/actions/test/integration-tests.js
+++ scripts/actions/test/integration-tests.js
@@ -2,30 +2,43 @@ const core = require('@actions/core')
 const runNpmScript = require('lib/helpers/run-npm-script')
 const chalk = require('chalk')
 
-process.env.STAGE = 'test'
-
 async function run () {
   try {
     const { output } = await runNpmScript('test:integration', { rejectOnNonZeroExitCode: false })
     // FIXME: this is a hack to get the output of the test script.
     // Even when all the tests passes, in the ci environment, the process returns a non-zero code.
     //
-    // check if stdout has a line that contains 'Tests:       {x} passed, {x} total'
-    // where {x} is a number and must be equal for passed and total
-    const regex = '/Tests:       (\\d+) passed, (\\d+) total/'
-    const match = output.match(regex)
-    if (match) {
-      const passed = Number(match[1])
-      const total = Number(match[2])
-      if (passed === total) {
-        core.setOutput('passed', passed)
-        core.setOutput('total', total)
-        process.exit(0)
-      } else {
-        core.setFailed(`Only ${chalk.yellow(passed)} of ${chalk.red(total)} tests passed.`)
-      }
+    // Extract the results from the output. The line should look like this:
+    // Tests:       57 failed, 2 passed, 59 total
+    // Or in case of success:
+    // Tests:       57 passed, 59 total
+    //
+    // Parse the line and get the thest numbers.
+    const testResults = output.split('\n').find(line => line.includes('Tests:  '))
+    const numbersInLine = testResults
+      .split(' ')
+      .filter(word => word !== '')
+      .filter(word => !isNaN(word))
+      .map(Number)
+    let failed = 0
+    let passed = 0
+    let total = 0
+    if (numbersInLine.length === 3) {
+      failed = numbersInLine[0]
+      passed = numbersInLine[1]
+      total = numbersInLine[2]
+    } else if (numbersInLine.length === 2) {
+      passed = numbersInLine[0]
+      total = numbersInLine[1]
+    } else {
+      throw new Error(`Unexpected test results: ${testResults}`)
     }
-    core.setFailed('Failed to parse stdout')
+    if (failed > 0) {
+      core.setFailed(`${chalk.red(`${failed} tests failed`)}`)
+    } else {
+      core.setOutput('tests-total', total)
+      core.setOutput('tests-passed-count', passed)
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
